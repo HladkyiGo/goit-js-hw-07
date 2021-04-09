f92f21fc3d23cdccae30c64266583fe7781e65e9
HladkyiGo <74014701+HladkyiGo@users.noreply.github.com>
2021-04-09 20:11:56 +0200

    7

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,12 +7,10 @@ const ingredients = [
   'Приправы',
 ];
 
-const liItem = document.createElement('li')
-const parenUl = document.getElementById('ingredients')
-parenUl.append(liItem)
-
-ingredients.forEach(ingredient => {
-    const liItem = document.createElement('li')
-    liItem.textContent = ingredient;
-    parenUl.append([...liItem])
+const ingridientsList = document.querySelector('#ingredients');
+const createEl = ingredients.map(ingredient => {
+  const element = document.createElement('li');
+  element.textContent = ingredient;
+  return element;
 })
+ingridientsList.append(...createEl)
